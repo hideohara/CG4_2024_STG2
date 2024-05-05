cfa78ddfce9bd73f119b92f42d9361734b6ed0ea
ohara <ohara@g.neec.ac.jp>
2024-05-05 23:27:07 +0900

    03_02_弾

--- Assets/BulletScript.cs
+++ Assets/BulletScript.cs
@@ -2,16 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManagerScript : MonoBehaviour
+public class BulletScript : MonoBehaviour
 {
+    public Rigidbody rb;
+    float moveSpeed = 8.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb.velocity = new Vector3(0, 0, moveSpeed);
+        Destroy (gameObject, 5);
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
         
     }
--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -6,6 +6,8 @@ public class PlayerScript : MonoBehaviour
 {
     public Rigidbody rb;
     public Animator animator;
+    public GameObject bullet;
+    private int bulletTimer = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -40,5 +42,26 @@ public class PlayerScript : MonoBehaviour
             rb.velocity = new Vector3(0, 0, 0);
         }
 
+        // ’e”­ŽË
+        if (bulletTimer == 0)
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                Vector3 position = transform.position;
+                position.y += 0.8f;
+                position.z += 0.5f;
+
+                Instantiate(bullet, position, Quaternion.identity);
+                bulletTimer = 1;
+            }
+        }
+        else
+        {
+            bulletTimer++;
+            if (bulletTimer > 20)
+            {
+                bulletTimer = 0;
+            }
+        }
     }
 }
